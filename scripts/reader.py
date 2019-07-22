a161839efbadf77f508075fc9dd192333761f8a8
Anvar Allagulov <payable@yandex.ru>
2019-07-22 22:08:48 +0300

    add parsing file per columns

--- scripts/reader.py
+++ scripts/reader.py
@@ -1,5 +1,14 @@
 import argparse
 from argparse import ArgumentParser
+import csv
+
+headers = [
+    'DATE',
+    'TIME1',
+    'TIME2',
+    'TICKET',
+    'COMMENT'
+]
 
 def parse_cli_args():
     parser = argparse.ArgumentParser()
@@ -10,10 +19,21 @@ def parse_cli_args():
     
     cli_args = parser.parse_args()
     return cli_args
-    
+
 if __name__ == '__main__':
     cli_args = parse_cli_args()
+    fname = cli_args.csv
+    
+    result = {}
+    for h in headers:
+        result[h] = []
 
-    print(cli_args.csv)
-
+    with open(fname, newline='') as csvfile:
+        tsh_reader = csv.reader(csvfile, delimiter=',')
+        for row in tsh_reader:
+            for h, v in zip(headers, row):
+                result[h].append(v)
+                
+    print(result)
+  
     exit(0)
