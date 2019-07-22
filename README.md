527ddb07ec506f08518e407ef743b3f74a8e1c1c
Anvar Allagulov <payable@yandex.ru>
2019-07-22 23:00:11 +0300

    add statistic outpit add usage example to readme

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # timesheet_analyse
 Just parsing logs and prepare some different slices
+
+Usage:
+>python3 scripts/reader.py --csv=timesheet_example/aallagulov.csv 
+seconds_at_all
+28800
+seconds_per_type
+{'QA': 1800, 'REQ': 2700, 'REVIEW': 3900, 'DEPLOY': 9900, 'DEV': 7200, 'FIX_REVIEW': 1800, 'OTHER': 1500}
+percents_per_type
+DEPLOY 34.38%
+DEV 25.00%
+REVIEW 13.54%
+REQ 9.38%
+QA 6.25%
+FIX_REVIEW 6.25%
+OTHER 5.21%
--- scripts/reader.py
+++ scripts/reader.py
@@ -1,6 +1,9 @@
 import argparse
 from argparse import ArgumentParser
 import csv
+from datetime import datetime
+
+FMT = '%H:%M'
 
 headers = [
     'DATE',
@@ -24,16 +27,44 @@ if __name__ == '__main__':
     cli_args = parse_cli_args()
     fname = cli_args.csv
     
-    result = {}
-    for h in headers:
-        result[h] = []
-
+    seconds_at_all = 0
+    seconds_per_type = {}
+    percents_per_type = {}
+    
+    result = []
     with open(fname, newline='') as csvfile:
         tsh_reader = csv.reader(csvfile, delimiter=',')
-        for row in tsh_reader:
+        filtered = filter(lambda line: (line and not line[0].isspace()), tsh_reader)
+        for row in filtered:
+            line_result = {}
             for h, v in zip(headers, row):
-                result[h].append(v)
+                line_result[h] = v
+            
+            time_delta = datetime.strptime(line_result['TIME2'], FMT) - datetime.strptime(line_result['TIME1'], FMT) 
+            secons = time_delta.seconds   
+            line_result['TIME_DELTA'] = secons
+            seconds_at_all += secons
+            
+            split_comment = line_result['COMMENT'].split(':')
+            tag = split_comment[0]
+            # checking only tagged comments
+            if tag and len(split_comment)>1:
+                line_result['TYPE'] = tag
                 
-    print(result)
+                current_time_delta = seconds_per_type.get(line_result['TYPE'], 0)
+                seconds_per_type[line_result['TYPE']] = current_time_delta + line_result['TIME_DELTA']
+                
+                result.append(line_result)
+ 
+    for t, s in seconds_per_type.items():
+        percents_per_type[t] = s/seconds_at_all
+                
+    print('seconds_at_all', end='\n')
+    print(seconds_at_all, end='\n')
+    print('seconds_per_type', end='\n')
+    print(seconds_per_type, end='\n')
+    print('percents_per_type', end='\n')
+    for t, p in sorted(percents_per_type.items(), key=lambda x: x[1], reverse=True):
+        print(t+" "+"{:.2%}".format(p), end='\n')
   
     exit(0)
